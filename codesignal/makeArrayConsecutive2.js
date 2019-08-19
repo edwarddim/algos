a553fb3df23e5364d6968c93a3d0887faf69f153
Edward <eddieim@gmail.com>
2019-08-19 15:10:57 -0700

    finished makeArrayConsecutive2

--- codesignal/makeArrayConsecutive2.js
+++ codesignal/makeArrayConsecutive2.js
@@ -1,13 +1,19 @@
 function makeArrayConsecutive2(statues) {
-    if(statues.length == 1){
-        return 0;
-    }
-    statues.sort();
-    var countSum = 0;
-    for(let i = 0; i < statues.length-1; i++){
-        countSum += statues[i+1] - statues[i] - 1;
-    }
-    return countSum
+    // if(statues.length == 1){
+    //     return 0;
+    // }
+    // statues.sort();
+    // var countSum = 0;
+    // for(let i = 0; i < statues.length-1; i++){
+    //     countSum += statues[i+1] - statues[i] - 1;
+    // }
+    // return countSum
+    statues.sort()
+    return statues[statues.length-1] - statues[0] + 1 - statues.length;
+}
+
+function makeArrayConsecutive2(sequence) {
+    return Math.max(...sequence)-Math.min(...sequence)+1-sequence.length
 }
 
 
