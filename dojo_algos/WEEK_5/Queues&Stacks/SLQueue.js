6d3ebabe22783a89e06600b025ae6d8b304e1334
Edward <eddieim@gmail.com>
2019-11-14 09:59:48 -0800

    Added Queue methods

--- dojo_algos/WEEK_5/Queues&Stacks/SLQueue.js
+++ dojo_algos/WEEK_5/Queues&Stacks/SLQueue.js
@@ -20,6 +20,8 @@ class SLQueue{
             runner = runner.next
         }
         console.log(runner.val)
+        console.log("HEAD IS: ", this.head)
+        console.log("TAIL IS: ", this.tail)
     }
     enqueue(val){
         if(this.head == null){
@@ -40,20 +42,75 @@ class SLQueue{
         this.head = this.head.next
         return returnVal;
     }
+    zipper(arr1, arr2){
+        this.head = new SLNode(arr1[0])
+        this.head.next = new SLNode(arr2[0])
+        this.tail = this.head.next
+        for(let i = 1; i < arr1.length; i++){
+            this.tail.next = new SLNode(arr1[i])
+            this.tail = this.tail.next
+            this.tail.next = new SLNode(arr2[i])
+            this.tail = this.tail.next
+        }
+    }
+    size(){
+        if(this.head == null){
+            return 0
+        }
+        var count = 1
+        var runner = this.head
+        while(runner.next != null){
+            count++
+            runner = runner.next
+        }
+        return count
+    }
+    interleave(){
+        if(this.head == null){
+            return false;
+        }
+        var rightRunner = this.head
+        var halfIndex = Math.floor(this.size()/2) - 1
+        var count = 0
+        var countIndex = 0
+        while(countIndex < halfIndex){
+            rightRunner = rightRunner.next
+            countIndex++
+        }
+        rightRunner = rightRunner.next
+
+        var leftRunner = this.head.next
+        this.head.next = rightRunner
+        this.tail = rightRunner
+        rightRunner = rightRunner.next
+        while(count < halfIndex){
+            this.tail.next = leftRunner
+            leftRunner = leftRunner.next
+            this.tail = this.tail.next
+
+            this.tail.next = rightRunner
+            rightRunner = rightRunner.next
+            this.tail = this.tail.next
+            
+            count++
+        }
+    }
 }
 module.exports = {
     queue : SLQueue
 }
 
-// const list = new SLQueue()
-// list.enqueue(1)
-// list.enqueue(2)
-// list.enqueue(3)
-// list.enqueue(4)
-// list.enqueue(5)
-// list.enqueue(6)
+const list = new SLQueue()
+// list.zipper([1,2,3] , [4,5,6])
+list.enqueue(1)
+list.enqueue(2)
+list.enqueue(3)
+list.enqueue(4)
+list.enqueue(5)
+list.enqueue(6)
 // console.log("---------------------------------")
-// list.printList()
 // console.log("returning from dequeue: ", list.dequeue())
-// console.log("---------------------------------")
-// list.printList()
+list.printList()
+console.log("---------------------------------")
+list.interleave()
+list.printList()
