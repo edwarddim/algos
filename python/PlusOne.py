0a8d1253536c91183443d0c39f70ad58a0bdd4ae
Edward Im <eddieim@gmail.com>
2023-12-21 17:37:51 -0800

    plus one

--- python/PlusOne.py
+++ python/PlusOne.py
@@ -1,3 +1,5 @@
+import math
+
 class Solution(object):
     def plusOne(self, digits):
         """
@@ -5,3 +7,11 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        digits.insert(0,1)
+        return digits
