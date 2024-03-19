4a00e9aa87c24a0979eebd83afaf3c5304d5015d
Edward Im <eddieim@gmail.com>
2024-03-19 11:14:17 -0700

    majorityElement

--- grind_75/majorityElement.py
+++ grind_75/majorityElement.py
@@ -1,3 +1,31 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
+        majority = len(nums) // 2
+        number_dictionary = {}
+        for num in nums:
+            if num in number_dictionary:
+                number_dictionary[num] += 1
+            else:
+                number_dictionary[num] = 1
+            if number_dictionary[num] > majority:
+                return num
+    # Sorting Solution
+    # def majorityElement(self, nums: List[int]) -> int:
+    #     nums.sort()
+    #     n = len(nums) // 2
+    #     return nums[n]
+    # Moore Voting Algo
+    #     def majorityElement(self, nums: List[int]) -> int:
+    #         count = 0
+    #         candidate = 0
+            
+    #         for num in nums:
+    #             if count == 0:
+    #                 candidate = num
+                
+    #             if num == candidate:
+    #                 count += 1
+    #             else:
+    #                 count -= 1
+            
+    #         return candidate
